c45546eba3a60a184ce008d328fa2d9c887c330d
Mariana Fidalgo <mariana.fidalgo.f@tecnico.ulisboa.pt>
2020-10-18 17:28:41 +0100

    Fixed mistake in peer-to-peer network

--- projeto1.c
+++ projeto1.c
@@ -48,7 +48,7 @@ int main() {
         else if (type == 2)
         {
             addEdge(graph, src, dest,n_nodes);
-            addEdge(graph, src, dest,n_nodes);
+            addEdge(graph, dest, src,n_nodes);
 
         }
     }
