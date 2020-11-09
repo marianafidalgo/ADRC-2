4cb96858f2f12462e399fc1384abaa2e2cb0309a
Mariana Fidalgo <32492467+marianafidalgo@users.noreply.github.com>
2020-11-09 00:16:33 +0000

    Update functions

--- functions.h
+++ functions.h
@@ -27,33 +27,19 @@ struct Queue {
   int tail;  //location to push from
 };
 
-struct StrongCC {
-  int id;
-  int * array;
-  struct StrongCC *next;
-};
-
 struct Graph *graph;
 struct Queue queue;
 
 struct node * createNode(int id_node, int type);
 struct Graph* createGraph();
 struct Queue* createQueue();
-struct StrongCC * createSCC();
 void addEdge(struct Graph* graph, int src, int dest, int type);
 void printGraph(struct Graph* graph);
 void BFS(struct Graph* graph, struct Queue * queue);
-void connected(struct Graph* graph, struct Queue * queue);
 int findTier1(struct Graph* graph);
 int pop_queue(struct Queue* queue);
 int push_queue(struct Queue* queue, int a);
-void DFSbridges(struct Graph* graph, int u, int disc[], int l[], int pred[], int *time, int *bi);
-void find_bridges(struct Graph* graph);
-int DFS_cycles(struct Graph* graph, int v, int curr, int curr_path[], int stack[]);
-void checkCycles(struct Graph* graph);
 int DFS_normal(struct Graph * graph, int v, int curr, int curr_path[], int stack[], int * last);
-void DFS_inversed(struct Graph * graph, int v, int *curr_path);
-void NoTier1_CommerciallyConn(struct Graph * graph);
 void CommerciallyConn(struct Graph* graph);
 void freeAll(struct Graph* graph, struct Queue * queue);
 
